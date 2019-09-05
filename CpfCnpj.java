0db11579cbdae6b7e2ba91e8cf03aa357142abdd
Clovis Gargione <clovis.gargione@gmail.com>
2019-09-05 14:26:34 -0300

    Create CpfCnpj.java

---
+++ CpfCnpj.java
@@ -0,0 +1,24 @@
+package ***;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = {CpfCnpjValidator.class})
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(value = RetentionPolicy.RUNTIME)
+@Documented
+public @interface CpfCnpj {
+
+	String message() default "O CPF/CNPJ é inválido";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
